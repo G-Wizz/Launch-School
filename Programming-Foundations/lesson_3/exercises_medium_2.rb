ec1ba65a24f4393a2f931efed696e02e6f352af5
Greg Schmitt <gregschmitt@Gregs-MBP.fios-router.home>
2016-12-29 09:51:50 -0500

    medium 2 exercises

---
+++ Programming-Foundations/lesson_3/exercises_medium_2.rb
@@ -0,0 +1,120 @@
+# Question 1
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" }
+}
+
+total_male_age = 0
+
+munsters.each do |name, details|
+  total_male_age += details["age"] if details["gender"] == "male"
+end
+
+puts total_male_age
+
+
+# Question 2
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |name, details|
+  puts "#{name} is a #{details["age"]} year old #{details["gender"]}."
+end
+
+#   Or
+
+munsters.each_pair do |name, details|
+  puts "#{name} is a #{details["age"]} year old #{details["gender"]}"
+end
+
+
+# Question 3
+
+def not_so_tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+
+  return a_string_param, an_array_param
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+my_string, my_array = not_so_tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+
+
+# Question 4
+
+sentence = "Humpty Dumpty sat on a wall."
+
+words = sentence.split(/\W/)
+ 
+words.reverse!
+
+new_sentence = words.join(" ") + "."
+
+puts new_sentence
+
+
+# Question 5
+
+# 34
+
+
+# Question 6
+
+# yes because it is getting passed into the method
+
+
+# Question 7
+
+def rps(fist1, fist2)
+  if fist1 == "rock"
+    (fist2 == "paper") ? "paper" : "rock"
+  elsif fist1 == "paper"
+    (fist2 == "scissors") ? "scissors" : "paper"
+  else
+    (fist2 == "rock") ? "rock" : "scissors"
+  end
+end
+
+puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
+
+# paper
+
+
+# Question 8
+
+# no
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
