17339888c58ede25dc98c34b0947e30c7ace3f27
Greg Schmitt <gregschmitt@Gregs-MBP.fios-router.home>
2016-12-31 08:46:16 -0500

    medium 3 exercises

---
+++ Programming-Foundations/lesson_3/exercises_medium_3.rb
@@ -0,0 +1,19 @@
+# Question 1 - see solution in question 1
+
+
+# Question 2 - see solution in question 2
+
+
+# Question 3 - see solution in question 3
+
+
+# Question 4 - see solution in question 4
+
+
+# Question 5
+
+def color_valid(color)
+  color == "blue" || color == "green"
+end
+
+
