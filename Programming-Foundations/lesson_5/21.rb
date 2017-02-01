4d593fa408e0b68bcf7031ca1deba475eeae2966
Greg Schmitt <gwizzprg100@gmail.com>
2017-02-01 02:54:54 -0500

    21 game code

--- Programming-Foundations/lesson_5/21.rb
+++ Programming-Foundations/lesson_5/21.rb
@@ -52,76 +52,122 @@ def total(cards)
   sum
 end
 
-# def display_card_format_player(hand)
-#   hand.map { |cards| cards[1] + ' of ' + cards[0] }.join(', ')
-# end
-
-# def display_card_format_dealer(hand)
-#   hand.map { |card| card[1] + ' of ' + card[0] }.join(', ')
-# end
-
-def join_with_of_player(hand)
-  hand.map { |cards| cards[1] + ' of ' + cards[0] }.join(', ')
-end
-
 def join_with_of_dealer(hand)
+  hand.map.with_index do |cards, index|
+    if index != 0
+      cards[1] + ' of ' + cards[0]
+    else
+      '?'
+    end
+  end.join(', ')
+end
+
+def join_with_of(hand)
   hand.map { |cards| cards[1] + ' of ' + cards[0] }.join(', ')
 end
 
-#main game loop
-system 'clear'
-deck = initialize_deck
-player_hand, dealer_hand = deal_cards(deck)
-
-player_total = 0
-dealer_total = 0
-
-prompt "Welcome to a game of 21!"
-prompt "========================"
-# player turn
-player_answer = nil
-loop do
-  player_total = total(player_hand)
-  dealer_total = total(dealer_hand)
-  prompt "Player has: #{join_with_of_player(player_hand)}"
-  prompt "Your total is: #{player_total}"
-  break if bust?(player_hand)
-  prompt "Dealer has: #{join_with_of_dealer(dealer_hand)} and ?"
-  prompt "Hit(h) or Stay(s)?"
-  player_answer = gets.chomp
-  system 'clear'
-  
-  break if player_answer.downcase.include?('s')
-  player_hand << deck.pop
-  
+def display_player_results(hand, total)
+  prompt "Player has: #{join_with_of(hand)}"
+  prompt "Your total is: #{total}"
 end
 
-if bust?(player_hand)
-  prompt "You busted! Game over.."
-else
-  prompt "You chose to stay with: #{join_with_of_player(player_hand)}"
-  prompt "A total of: #{player_total}"
+def display_dealer_results(hand, total)
+  prompt "Dealer has: #{join_with_of_dealer(hand)}"
+  prompt "With a total of: #{total}"
 end
 
-# dealer turn
-
-loop do
-  prompt "Dealer has: #{join_with_of_dealer(dealer_hand)}"
-  sleep(2)
-  break if bust?(dealer_hand) || total(dealer_hand) >= STAY
-  if total(dealer_hand) < STAY
-    prompt "Dealer chose to hit."
-    dealer_hand << deck.pop
+def calculate_winner(player_hand, dealer_hand)
+  if total(player_hand) > total(dealer_hand) || bust?(dealer_hand)
+    'player'
+  elsif total(dealer_hand) > total(player_hand) || bust?(player_hand)
+    'dealer'
+  else total(dealer_hand) == total(player_hand)
+    'tie'
   end
 end
 
-if bust?(dealer_hand)
-  prompt "Dealer busted! Game over.."
-else
-  prompt "Dealer chose to stay."
+def display_winner(winner)
+  case winner
+  when 'player'
+    prompt "You Win!"
+  when 'dealer'
+    prompt "Dealer Wins!"
+  else
+    prompt "It's a tie!"
+  end
 end
 
+# main game loop
+loop do
+  system 'clear'
+  deck = initialize_deck
+  player_hand, dealer_hand = deal_cards(deck)
+
+  player_total = 0
+  dealer_total = 0
+
+  prompt "Welcome to a game of 21!"
+  prompt "========================"
+  prompt "Dealing cards....."
+  sleep(2)
+  # player turn
+  player_answer = nil
+
+  loop do
+    player_total = total(player_hand)
+    display_player_results(player_hand, player_total)
+    break if bust?(player_hand)
+    prompt "Dealer has: #{join_with_of_dealer(dealer_hand)}"
+    prompt "Hit(h) or Stay(s)?"
+    player_answer = gets.chomp
+    system 'clear'
+    break if player_answer.downcase.include?('s')
+    player_hand << deck.pop
+  end
+
+  if bust?(player_hand)
+    prompt "You busted! Game over.."
+  else
+    prompt "You chose to stay with: #{join_with_of(player_hand)}"
+    prompt "A total of: #{player_total}"
+  end
+
+  # dealer turn
+    prompt "Dealer has: #{join_with_of_dealer(dealer_hand)}"
+  loop do
+    
+    sleep(2)
+    
+    if total(dealer_hand) < STAY
+      prompt "Dealer chose to hit with: #{join_with_of(dealer_hand)}"
+      dealer_hand << deck.pop
+    end
+    
+    break if bust?(dealer_hand) || total(dealer_hand) >= STAY
+  end
+  dealer_total = total(dealer_hand)
+
+  if bust?(dealer_hand)
+    prompt "Dealer busted! Game over.."
+    prompt "Dealer has: #{join_with_of(dealer_hand)}"
+    prompt "With a total of: #{dealer_total}"
+  else
+    prompt "Dealer chose to stay with: #{join_with_of(dealer_hand)}"
+    prompt "With a total of: #{dealer_total}"
+  end
+
+  winner = calculate_winner(player_hand, dealer_hand)
+  display_winner(winner)
+
+  prompt "Would you like to play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.include?('y')
+
+end
+prompt "Thank you for playing 21! Good Bye"
 
 
+# create method to compare and determine winner
+# refactor and consolidate repititious code into methods
 
 
